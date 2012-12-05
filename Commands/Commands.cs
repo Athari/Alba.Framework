28990075fd9aea7cdab8ef3effe1abfb356b7c67
Athari <athari.p@gmail.com>
2012-12-06 03:11:31 +0400

    Added command bindings

--- Commands/Commands.cs
+++ Commands/Commands.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Windows;
 using System.Windows.Input;
+using Alba.Framework.Linq;
+using DpChangedEventArgs = System.Windows.DependencyPropertyChangedEventArgs;
 
 namespace Alba.Framework.Commands
 {
     public static class Commands
     {
+        private static readonly KeyGestureConverter _keyGestureConverter = new KeyGestureConverter();
+
         public static readonly DependencyProperty CommandProperty =
             DependencyProperty.RegisterAttached("Command", typeof(CommandDisplay), typeof(Commands),
                 new PropertyMetadata(CommandProperty_Changed));
+        public static readonly DependencyProperty CommandBindingsProperty =
+            DependencyProperty.RegisterAttached("CommandBindings", typeof(IEnumerable<CommandBinding>), typeof(Commands),
+                new PropertyMetadata(CommandBindingsProperty_Changed));
 
         public static void SetCommand (DependencyObject d, CommandDisplay value)
         {
@@ -21,11 +29,36 @@ namespace Alba.Framework.Commands
             return (CommandDisplay)d.GetValue(CommandProperty);
         }
 
-        private static void CommandProperty_Changed (DependencyObject d, DependencyPropertyChangedEventArgs e)
+        public static void SetCommandBindings (DependencyObject d, IEnumerable<CommandBinding> value)
         {
-            Type type = d.GetType();
-            RoutedCommand command = ((CommandDisplay)e.NewValue).Command;
-            DependencyPropertyDescriptor.FromName("Command", type, type).SetValue(d, command);
+            d.SetValue(CommandBindingsProperty, value);
+        }
+
+        public static IEnumerable<CommandBinding> GetCommandBindings (DependencyObject d)
+        {
+            return (IEnumerable<CommandBinding>)d.GetValue(CommandBindingsProperty);
+        }
+
+        private static void CommandProperty_Changed (DependencyObject d, DpChangedEventArgs e)
+        {
+            Properties.SetDpValue(d, "Command", ((CommandDisplay)e.NewValue).Command);
+        }
+
+        private static void CommandBindingsProperty_Changed (DependencyObject d, DpChangedEventArgs e)
+        {
+            var bindings = Properties.GetValue<CommandBindingCollection>(d, "CommandBindings");
+            foreach (var binding in (IEnumerable<CommandBinding>)e.NewValue)
+                bindings.Add(binding);
+        }
+
+        public static RoutedCommand RegisterCommand (Type ownerType, Expression<Func<RoutedCommand>> propExpr, params string[] gestures)
+        {
+            var cmd = new RoutedCommand(Properties.GetName(propExpr), ownerType);
+            // ReSharper disable AssignNullToNotNullAttribute (converter throws if can't convert)
+            foreach (string gesture in gestures)
+                cmd.InputGestures.Add((InputGesture)_keyGestureConverter.ConvertFrom(gesture));
+            // ReSharper restore AssignNullToNotNullAttribute
+            return cmd;
         }
     }
 }
---
+++ Linq/Properties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Windows;
+
+namespace Alba.Framework.Linq
+{
+    public static class Properties
+    {
+        public static string GetName<TObj, TProp> (Expression<Func<TObj, TProp>> expr)
+        {
+            return ((MemberExpression)expr.Body).Member.Name;
+        }
+
+        public static string GetName<TProp> (Expression<Func<TProp>> expr)
+        {
+            return ((MemberExpression)expr.Body).Member.Name;
+        }
+
+        public static T SetDpValue<T> (DependencyObject d, string propName, T value)
+        {
+            Type type = d.GetType();
+            DependencyPropertyDescriptor.FromName(propName, type, type).SetValue(d, value);
+            return value;
+        }
+
+        public static T GetDpValue<T> (DependencyObject d, string propName)
+        {
+            Type type = d.GetType();
+            return (T)DependencyPropertyDescriptor.FromName(propName, type, type).GetValue(d);
+        }
+
+        public static T SetValue<T> (object o, string propName, T value)
+        {
+            o.GetType().GetProperty(propName).SetValue(o, value);
+            return value;
+        }
+
+        public static T GetValue<T> (object o, string propName)
+        {
+            return (T)o.GetType().GetProperty(propName).GetValue(o);
+        }
+    }
+}
