f9d56fe17016f540fe839298ac35c9e927ddbd8f
Athari <athari.p@gmail.com>
2013-01-02 12:33:25 +0400

    Added hackish type resolving to AncestorBinding

--- Markup/AncestorBinding.cs
+++ Markup/AncestorBinding.cs
@@ -1,18 +1,41 @@
 ﻿using System;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Alba.Framework.Markup
 {
     public class AncestorBinding : Binding
     {
-        public AncestorBinding (Type ancestor)
+        public AncestorBinding (string path) : base(path)
         {
-            RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, ancestor, 1);
+            if (!IsPropertyReference(path))
+                throw new ArgumentException(string.Format("Ancestor type not specified in path '{0}'.", path), "path");
+            string propRef = path.Substring(1, path.IndexOf(')') - 1);
+            string ownerTypeName = propRef.Substring(0, propRef.LastIndexOf('.')).Trim();
+            Type ownerType = GetTypeFromName(Path, ownerTypeName);
+            if (ownerType == null)
+                throw new ArgumentException(string.Format("Ancestor type '{0}' not found using path '{1}'.",
+                    ownerTypeName, path), "path");
+            RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, ownerType, 1);
         }
 
         public AncestorBinding (string path, Type ancestor) : base(path)
         {
             RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, ancestor, 1);
+        }
+
+        private static bool IsPropertyReference (string name)
+        {
+            return name != null && name.Length > 1 && name[0] == '(' && name.IndexOf('.') != -1;
+        }
+
+        private static Type GetTypeFromName (PropertyPath propertyPath, string name)
+        {
+            // HACK
+            return (Type)typeof(PropertyPath)
+                .GetMethod("GetTypeFromName", BindingFlags.Instance | BindingFlags.NonPublic)
+                .Invoke(propertyPath, new object[] { name, null });
         }
     }
 }
