0decd4cbc046b3d890cc5de1f8e4fb112e5d7ef9
Athari <athari.p@gmail.com>
2013-02-18 05:07:27 +0400

    Added ErrorContentConverter, DefaultStyleExtension

---
+++ Markup/DefaultStyleExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows;
+
+namespace Alba.Framework.Markup
+{
+    public class DefaultStyleExtension : StaticResourceExtension
+    {
+        public DefaultStyleExtension (Type type) : base(type)
+        {}
+    }
+}
