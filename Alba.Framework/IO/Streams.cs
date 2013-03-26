21f986d1422e3d5f658e10b9f2cda7b2015d313d
Athari <athari.p@gmail.com>
2013-03-26 17:46:53 +0400

    Added Streams class (file, string and memory stream helpers) and Encodings class (encoding constants without bom)

---
+++ Alba.Framework/IO/Streams.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Alba.Framework.Text;
+
+namespace Alba.Framework.IO
+{
+    public class Streams
+    {
+        private const FileShare DefaultFileShare = FileShare.Read;
+        //private const int DefaultBufferSize = 1024;
+
+        public static FileStream CreateFile (string path)
+        {
+            return new FileStream(path, FileMode.Create, FileAccess.Write, DefaultFileShare);
+        }
+
+        public static FileStream AppendFile (string path)
+        {
+            return new FileStream(path, FileMode.Append, FileAccess.Write, DefaultFileShare);
+        }
+
+        public static FileStream ReadFile (string path)
+        {
+            return new FileStream(path, FileMode.Open, FileAccess.Read, DefaultFileShare);
+        }
+
+        public static StreamWriter CreateTextFile (string path, Encoding encoding = null)
+        {
+            return new StreamWriter(path, false, encoding ?? Encodings.Utf8NoBom);
+        }
+
+        public static StreamWriter AppendTextFile (string path, Encoding encoding = null)
+        {
+            return new StreamWriter(path, true, encoding ?? Encodings.Utf8NoBom);
+        }
+
+        public static StreamReader ReadTextFile (string path, Encoding encoding = null)
+        {
+            return new StreamReader(path, encoding ?? Encodings.Utf8);
+        }
+
+        public static BinaryWriter CreateBinaryFile (string path, Encoding encoding = null)
+        {
+            return new BinaryWriter(CreateFile(path), encoding ?? Encodings.Utf8NoBom);
+        }
+
+        public static BinaryWriter AppendBinaryFile (string path, Encoding encoding = null)
+        {
+            return new BinaryWriter(AppendFile(path), encoding ?? Encodings.Utf8NoBom);
+        }
+
+        public static BinaryReader ReadBinaryFile (string path, Encoding encoding = null)
+        {
+            return new BinaryReader(ReadFile(path), encoding ?? Encodings.Utf8NoBom);
+        }
+
+        public static StringWriter WriteString (IFormatProvider formatProvider = null)
+        {
+            return new StringWriter(formatProvider ?? CultureInfo.CurrentCulture);
+        }
+
+        public static StringReader ReadString (string value, Encoding encoding = null)
+        {
+            return new StringReader(value);
+        }
+
+        public static StringWriter WriteStringBuilder (StringBuilder sb, IFormatProvider formatProvider = null)
+        {
+            return new StringWriter(sb, formatProvider ?? CultureInfo.CurrentCulture);
+        }
+
+        public static MemoryStream WriteMemory (int capacity = 0)
+        {
+            return new MemoryStream(capacity);
+        }
+
+        public static MemoryStream WriteMemory (byte[] buffer, int index = 0, int count = -1)
+        {
+            return new MemoryStream(buffer, index, count != -1 ? count : buffer.Length - index, true);
+        }
+
+        public static MemoryStream ReadMemory (byte[] buffer, int index = 0, int count = -1)
+        {
+            return new MemoryStream(buffer, index, count != -1 ? count : buffer.Length - index, false);
+        }
+    }
+}
---
+++ Alba.Framework/Text/Encodings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace Alba.Framework.Text
+{
+    public static class Encodings
+    {
+        private static readonly Lazy<Encoding> _utf8 = new Lazy<Encoding>(() => new UTF8Encoding(false, true));
+        private static readonly Lazy<Encoding> _utf8Safe = new Lazy<Encoding>(() => new UTF8Encoding(false, false));
+        private static readonly Lazy<Encoding> _utf8NoBom = new Lazy<Encoding>(() => new UTF8Encoding(false, true));
+        private static readonly Lazy<Encoding> _utf8NoBomSafe = new Lazy<Encoding>(() => new UTF8Encoding(false, false));
+        private static readonly Lazy<Encoding> _utf16 = new Lazy<Encoding>(() => new UnicodeEncoding(false, false, true));
+        private static readonly Lazy<Encoding> _utf16Safe = new Lazy<Encoding>(() => new UnicodeEncoding(false, false, false));
+        private static readonly Lazy<Encoding> _utf16NoBom = new Lazy<Encoding>(() => new UnicodeEncoding(false, false, true));
+        private static readonly Lazy<Encoding> _utf16NoBomSafe = new Lazy<Encoding>(() => new UnicodeEncoding(false, false, true));
+
+        public static Encoding Utf8
+        {
+            get { return _utf8.Value; }
+        }
+
+        public static Encoding Utf8Safe
+        {
+            get { return _utf8Safe.Value; }
+        }
+
+        public static Encoding Utf8NoBom
+        {
+            get { return _utf8NoBom.Value; }
+        }
+
+        public static Encoding Utf8NoBomSafe
+        {
+            get { return _utf8NoBomSafe.Value; }
+        }
+
+        public static Encoding Utf16
+        {
+            get { return _utf16.Value; }
+        }
+
+        public static Encoding Utf16Safe
+        {
+            get { return _utf16Safe.Value; }
+        }
+
+        public static Encoding Utf16NoBom
+        {
+            get { return _utf16NoBom.Value; }
+        }
+
+        public static Encoding Utf16NoBomSafe
+        {
+            get { return _utf16NoBomSafe.Value; }
+        }
+    }
+}
