aadcf853933d10f43a3ca4813b4a936523ae6b41
Athari <athari.p@gmail.com>
2013-03-27 16:54:16 +0400

    Updated EnumerableExts (added list, tree and graph traversal)

--- Alba.Framework/Collections/EnumerableExts.cs
+++ Alba.Framework/Collections/EnumerableExts.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Alba.Framework.Collections
 {
@@ -22,6 +23,58 @@ namespace Alba.Framework.Collections
         public static string ConcatString (this IEnumerable<string> @this)
         {
             return string.Concat(@this);
+        }
+
+        public static IEnumerable<T> TraverseList<T> (this T root, Func<T, T> getNext) where T : class
+        {
+            for (T current = root; current != null; current = getNext(current))
+                yield return current;
+        }
+
+        public static IEnumerable<T> TraverseTree<T> (this T root, Func<T, IEnumerable<T>> getChildren)
+        {
+            return TraverseTreeDepth(root, getChildren);
+        }
+
+        public static IEnumerable<T> TraverseTreeDepth<T> (this T root, Func<T, IEnumerable<T>> getChildren)
+        {
+            var stack = new Stack<T>();
+            stack.Push(root);
+
+            while (stack.Count != 0) {
+                T item = stack.Pop();
+                yield return item;
+                foreach (var child in getChildren(item))
+                    stack.Push(child);
+            }
+        }
+
+        public static IEnumerable<T> TraverseTreeBreadth<T> (this T root, Func<T, IEnumerable<T>> getChildren)
+        {
+            var queue = new Queue<T>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0) {
+                T item = queue.Dequeue();
+                yield return item;
+                queue.EnqueueRange(getChildren(item));
+            }
+        }
+
+        public static IEnumerable<T> TraverseGraph<T> (this T root, Func<T, IEnumerable<T>> getChildren)
+        {
+            var seen = new HashSet<T>();
+            var stack = new Stack<T>();
+            stack.Push(root);
+
+            while (stack.Count != 0) {
+                T item = stack.Pop();
+                if (seen.Contains(item))
+                    continue;
+                seen.Add(item);
+                yield return item;
+                stack.PushRange(getChildren(item));
+            }
         }
     }
 }
