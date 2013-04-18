d3b6c787458b68c3fca3beddcffd284050cfc9b4
Athari <athari.p@gmail.com>
2013-04-18 16:15:37 +0400

    Added PureAttribute to ObjectExts methods

--- Alba.Framework/Sys/ObjectExts.cs
+++ Alba.Framework/Sys/ObjectExts.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
 namespace Alba.Framework.Sys
 {
     public static class ObjectExts
     {
+        [Pure]
         public static bool Compare<T> (T x, T y, Func<bool> compare)
         {
             if (ReferenceEquals(x, y))
@@ -14,36 +16,43 @@ namespace Alba.Framework.Sys
             return compare();
         }
 
+        [Pure]
         public static bool EqualsAny<T> (this T @this, params T[] values)
         {
             return values.Any(v => Equals(@this, v));
         }
 
+        [Pure]
         public static string GetTypeFullName (this object @this)
         {
             return @this == null ? "null" : @this.GetType().FullName;
         }
 
+        [Pure]
         public static T To<T> (this object @this)
         {
             return (T)@this;
         }
 
+        [Pure]
         public static string NullableToString (this object @this)
         {
             return @this == null ? "" : @this.ToString();
         }
 
+        [Pure]
         public static bool IsAnyType (this object @this, params Type[] types)
         {
             return @this == null || types.Any(@this.GetType().IsAssignableTo);
         }
 
+        [Pure]
         public static bool IsAnyType<T1> (this object @this)
         {
             return @this is T1;
         }
 
+        [Pure]
         public static bool IsAnyType<T1, T2> (this object @this)
         {
             return @this is T1 || @this is T2;
