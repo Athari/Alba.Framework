37e4442de77802b98eb3df692628a789aaa65301
Athari <athari.p@gmail.com>
2013-04-18 19:37:18 +0400

    Added Alba.Framework.Testing project

---
+++ Alba.Framework.Testing/TraceRedirectTraceListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Alba.Framework.Testing
+{
+    public class TraceRedirectTraceListener : TraceListener
+    {
+        public override void Write (string message)
+        {
+            Trace.Write(message);
+        }
+
+        public override void WriteLine (string message)
+        {
+            Trace.WriteLine(message);
+        }
+    }
+}
