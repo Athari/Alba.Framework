2a66f4d3ba0b45d13bfb6f020a8fe3f2ff65960e
Athari <athari.p@gmail.com>
2013-05-07 18:33:49 +0400

    Added more EnumerableExts.ToDictionary overloads, changed returned types of ToReadOnly* from interfaces to concrete types

--- Alba.Framework/Collections/EnumerableExts.cs
+++ Alba.Framework/Collections/EnumerableExts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -57,33 +58,43 @@ namespace Alba.Framework.Collections
             return string.Concat(@this);
         }
 
-        public static IReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
+        public static Dictionary<TKey, T> ToDictionary<TKey, T> (this IEnumerable<KeyValuePair<TKey, T>> @this)
+        {
+            return @this.ToDictionary(p => p.Key, p => p.Value);
+        }
+
+        public static Dictionary<TKey, T> ToDictionary<TKey, T> (this IEnumerable<DictionaryEntry> @this)
+        {
+            return @this.ToDictionary(p => (TKey)p.Key, p => (T)p.Value);
+        }
+
+        public static ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
             Func<T, TKey> keySelector)
         {
             return new ReadOnlyDictionary<TKey, T>(@this.ToDictionary(keySelector));
         }
 
-        public static IReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
+        public static ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
             Func<T, TKey> keySelector, IEqualityComparer<TKey> comparer)
         {
             return new ReadOnlyDictionary<TKey, T>(@this.ToDictionary(keySelector, comparer));
         }
 
-        public static IReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
+        public static ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
             Func<T, TKey> keySelector, Func<T, T> elementSelector)
         {
             return new ReadOnlyDictionary<TKey, T>(@this.ToDictionary(keySelector, elementSelector));
         }
 
-        public static IReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
+        public static ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey, T> (this IEnumerable<T> @this,
             Func<T, TKey> keySelector, Func<T, T> elementSelector, IEqualityComparer<TKey> comparer)
         {
             return new ReadOnlyDictionary<TKey, T>(@this.ToDictionary(keySelector, elementSelector, comparer));
         }
 
-        public static IReadOnlyList<T> ToReadOnlyList<T> (this IEnumerable<T> @this)
+        public static ReadOnlyCollection<T> ToReadOnlyList<T> (this IEnumerable<T> @this)
         {
-            return new ReadOnlyCollection<T>(@this.ToList());
+            return @this.ToList().AsReadOnly();
         }
 
         public static IEnumerable<T> TraverseList<T> (this T root, Func<T, T> getNext) where T : class
