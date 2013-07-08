5e9511d29266d33c65ea498198a7cc4a075e3cbc
Athari <athari.p@gmail.com>
2013-07-08 05:08:48 +0400

    Added task schedulers from ParallelExtensionsExtras from Samples for Parallel Programming (http://code.msdn.microsoft.com/ParExtSamples): LimitedConcurrencyTaskScheduler, OrderedTaskScheduler (limited to one thread LimitedConcurrencyTaskScheduler), ApartmentTaskScheduler (based on StaTaskScheduler), ThreadTaskScheduler (limited to one thread ApartmentTaskScheduler)

---
+++ Alba.Framework/Threading/Tasks/Schedulers/OrderedTaskScheduler.cs
@@ -0,0 +1,13 @@
+﻿namespace Alba.Framework.Threading.Tasks.Schedulers
+{
+    /// <summary>
+    /// Provides a task scheduler that ensures only one task is executing at a time, and that tasks
+    /// execute in the order that they were queued. Uses threads from ThreadPool.
+    /// </summary>
+    public sealed class OrderedTaskScheduler : LimitedConcurrencyTaskScheduler
+    {
+        /// <summary>Initializes an instance of the OrderedTaskScheduler class.</summary>
+        public OrderedTaskScheduler () : base(1)
+        {}
+    }
+}
---
+++ Alba.Framework/Threading/Tasks/Schedulers/ThreadTaskScheduler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace Alba.Framework.Threading.Tasks.Schedulers
+{
+    /// <summary>
+    /// Provides a task scheduler that ensures only one task is executing at a time, and that tasks
+    /// execute in the order that they were queued. Uses a single thread.
+    /// </summary>
+    public sealed class ThreadTaskScheduler : ApartmentTaskScheduler
+    {
+        public ThreadTaskScheduler (ApartmentState apartmentState = ApartmentState.MTA, string threadName = "ThreadTaskScheduler")
+            : base(1, apartmentState, threadName)
+        {}
+    }
+}
