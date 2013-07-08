7cab0fbcdba4ebb6f2b0557606cb0bb919eea914
Athari <athari.p@gmail.com>
2013-07-09 03:56:26 +0400

    Added DictionaryExts.GetOrException

--- Alba.Framework/Collections/DictionaryExts.cs
+++ Alba.Framework/Collections/DictionaryExts.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using Alba.Framework.Sys;
+using Alba.Framework.Text;
 
 namespace Alba.Framework.Collections
 {
     public static class DictionaryExts
     {
-        public static void AddRange<TKey, TValue> (this Dictionary<TKey, TValue> @this,
-            IEnumerable<KeyValuePair<TKey, TValue>> items)
+        private const string ErrorKeyNotFound = "Key '{0}' not found.";
+
+        public static void AddRange<TKey, TValue> (this Dictionary<TKey, TValue> @this, IEnumerable<KeyValuePair<TKey, TValue>> items)
         {
             foreach (var kvp in items)
                 @this.Add(kvp.Key, kvp.Value);
@@ -19,29 +22,54 @@ namespace Alba.Framework.Collections
             return @this.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
         }
 
-        public static TValue GetOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> @this,
-            TKey key, TValue defaultValue = default(TValue))
+        public static TValue GetOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> @this, TKey key, TValue defaultValue = default(TValue))
         {
             TValue value;
             return @this.TryGetValue(key, out value) ? value : defaultValue;
         }
 
-        public static TValue GetOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> @this,
-            TKey key, Func<TValue> getDefaultValue)
+        public static TValue GetOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> @this, TKey key, Func<TValue> getDefaultValue)
         {
             TValue value;
             return @this.TryGetValue(key, out value) ? value : getDefaultValue();
         }
 
-        public static TValue? GetOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> @this,
-            TKey key, TValue? defaultValue = null) where TValue : struct
+        public static TValue? GetOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> @this, TKey key, TValue? defaultValue = null)
+            where TValue : struct
         {
             TValue value;
             return @this.TryGetValue(key, out value) ? value : defaultValue;
         }
 
-        public static TValue GetOrAdd<TKey, TValue> (this IDictionary<TKey, TValue> @this,
-            TKey key, Func<TValue> getDefaultValue)
+        public static TValue GetOrException<TKey, TValue> (this IDictionary<TKey, TValue> @this, TKey key, Type exceptionType = null)
+        {
+            TValue value;
+            if (@this.TryGetValue(key, out value))
+                return value;
+            else
+                throw ExceptionExts.Create(exceptionType ?? typeof(KeyNotFoundException), ErrorKeyNotFound.Fmt(key));
+        }
+
+        public static TValue GetOrException<TKey, TValue, TException> (this IDictionary<TKey, TValue> @this, TKey key)
+            where TException : Exception
+        {
+            TValue value;
+            if (@this.TryGetValue(key, out value))
+                return value;
+            else
+                throw ExceptionExts.Create<TException>(ErrorKeyNotFound.Fmt(key));
+        }
+
+        public static TValue GetOrException<TKey, TValue> (this IDictionary<TKey, TValue> @this, TKey key, Func<TKey, Exception> createException)
+        {
+            TValue value;
+            if (@this.TryGetValue(key, out value))
+                return value;
+            else
+                throw createException(key);
+        }
+
+        public static TValue GetOrAdd<TKey, TValue> (this IDictionary<TKey, TValue> @this, TKey key, Func<TValue> getDefaultValue)
         {
             TValue value;
             if (!@this.TryGetValue(key, out value))
