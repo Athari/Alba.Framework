4d1460439ba15eaff83d2b803a0578a3d4b47098
Athari <athari.p@gmail.com>
2013-07-16 18:34:43 +0400

    Added NotConverter

---
+++ Alba.Framework.Presentation/Windows/Markup/Converters/NotConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Alba.Framework.Windows.Markup
+{
+    [ValueConversion (typeof(bool), typeof(bool))]
+    public class NotConverter : IValueConverter
+    {
+        public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (ReferenceEquals(value, DependencyProperty.UnsetValue))
+                return DependencyProperty.UnsetValue;
+            return !(bool)value;
+        }
+
+        public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (ReferenceEquals(value, DependencyProperty.UnsetValue))
+                return DependencyProperty.UnsetValue;
+            return !(bool)value;
+        }
+    }
+}
