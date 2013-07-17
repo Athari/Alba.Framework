7287333a53bdf2d48aa1020843c418b2c9c499f7
Athari <athari.p@gmail.com>
2013-07-17 18:50:45 +0400

    Added ErrorContentConverter.IsMultiple

--- Alba.Framework.Presentation/Windows/Markup/Converters/ErrorContentConverter.cs
+++ Alba.Framework.Presentation/Windows/Markup/Converters/ErrorContentConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,13 +11,17 @@ namespace Alba.Framework.Windows.Markup
     [ValueConversion (typeof(ReadOnlyObservableCollection<ValidationError>), typeof(object))]
     public class ErrorContentConverter : IValueConverter
     {
+        public bool IsMultiple { get; set; }
+
         public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (ReferenceEquals(value, DependencyProperty.UnsetValue))
                 return DependencyProperty.UnsetValue;
 
             var errors = (ReadOnlyObservableCollection<ValidationError>)value;
-            return errors.Count > 0 ? errors[0].ErrorContent : null;
+            if (errors.Count == 0)
+                return null;
+            return IsMultiple ? errors.Select(e => e.ErrorContent) : errors.Single().ErrorContent;
         }
 
         public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
