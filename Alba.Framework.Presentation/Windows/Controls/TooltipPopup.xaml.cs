f18f757e16ff694cefacf2715c7d423c71098ffe
Athari <athari.p@gmail.com>
2013-07-17 18:56:14 +0400

    Added support for multiple error messages to TooltipPopup (also error messages as strings, ValidationErrors and ValidationMessages)

--- Alba.Framework.Presentation/Windows/Controls/TooltipPopup.xaml.cs
+++ Alba.Framework.Presentation/Windows/Controls/TooltipPopup.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Alba.Framework.Windows.Mvvm;
 
 namespace Alba.Framework.Windows.Controls
 {
@@ -14,6 +19,23 @@ namespace Alba.Framework.Windows.Controls
         private void TooltipPopup_MouseDown (object sender, MouseButtonEventArgs e)
         {
             Hide();
+        }
+    }
+
+    internal class TooltipPopupErrorContentTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate (object item, DependencyObject container)
+        {
+            var resources = (FrameworkElement)container;
+            if (item is String)
+                return (DataTemplate)resources.FindResource("tplString");
+            if (item is ValidationError)
+                return (DataTemplate)resources.FindResource("tplError");
+            if (item is ValidationMessage)
+                return (DataTemplate)resources.FindResource("tplMessage");
+            if (item is IEnumerable<object>) // ErrorContentConverter does not know type of error content and cannot cast to IEnumerable<ValidationMessage>
+                return (DataTemplate)resources.FindResource("tplMessages");
+            return null;
         }
     }
 }
