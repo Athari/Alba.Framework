a5444ee7d541c2f9243f2452e080d160f17327ae
Athari <athari.p@gmail.com>
2013-09-06 00:07:49 +0400

    Added UndisposableStream, StreamExts.ToUndisposable

---
+++ Alba.Framework/IO/StreamExts.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Alba.Framework.IO
+{
+    public static class StreamExts
+    {
+        public static Stream ToUndisposable (this Stream @this)
+        {
+            return new UndisposableStream(@this);
+        }
+    }
+}
