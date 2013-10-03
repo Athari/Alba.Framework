d53a08abf3510a95660607bd0bb6bbdb061f5a10
Athari <athari.p@gmail.com>
2013-10-03 21:45:24 +0400

    Added WrapView (WRapPanel view for ListView)

--- Alba.Framework.Presentation/Properties/AssemblyInfo.cs
+++ Alba.Framework.Presentation/Properties/AssemblyInfo.cs
@@ -1,10 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Alba.Framework.Presentation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,24 +12,9 @@ using System.Windows.Markup;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("80f5c964-0da5-4913-b7d1-ed0ef4d9e9e0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
@@ -41,3 +24,4 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("urn:alba:framework", "Alba.Framework.Windows.Media")]
 [assembly: XmlnsDefinition("urn:alba:framework", "Alba.Framework.Windows.Media.Animation")]
 [assembly: XmlnsDefinition("urn:alba:framework", "Alba.Framework.Windows.Mvvm")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
---
+++ Alba.Framework.Presentation/Windows/Controls/WrapView.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Alba.Framework.Windows.Controls
+{
+    public class WrapView : ViewBase
+    {
+        public static ComponentResourceKey WrapViewStyleKey = new ComponentResourceKey(typeof(WrapView), "styWrapView");
+
+        public static readonly DependencyProperty ItemContainerStyleProperty = ItemsControl.ItemContainerStyleProperty.AddOwner(typeof(WrapView));
+        public static readonly DependencyProperty ItemTemplateProperty = ItemsControl.ItemTemplateProperty.AddOwner(typeof(WrapView));
+        public static readonly DependencyProperty ItemWidthProperty = WrapPanel.ItemWidthProperty.AddOwner(typeof(WrapView));
+        public static readonly DependencyProperty ItemHeightProperty = WrapPanel.ItemHeightProperty.AddOwner(typeof(WrapView));
+
+        public Style ItemContainerStyle
+        {
+            get { return (Style)GetValue(ItemContainerStyleProperty); }
+            set { SetValue(ItemContainerStyleProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public double ItemWidth
+        {
+            get { return (double)GetValue(ItemWidthProperty); }
+            set { SetValue(ItemWidthProperty, value); }
+        }
+
+        public double ItemHeight
+        {
+            get { return (double)GetValue(ItemHeightProperty); }
+            set { SetValue(ItemHeightProperty, value); }
+        }
+
+        protected override object DefaultStyleKey
+        {
+            get { return WrapViewStyleKey; }
+        }
+    }
+}
