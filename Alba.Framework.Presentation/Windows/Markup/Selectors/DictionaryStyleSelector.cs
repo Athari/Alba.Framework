2393fe3dfa6bf7461a45f3d5c4c4d52d466a0be2
Athari <athari.p@gmail.com>
2013-11-14 20:35:40 +0400

    Added DictionaryStyleSelector and LazyExpand<T>.

---
+++ Alba.Framework.Presentation/Windows/Markup/Selectors/DictionaryStyleSelector.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Alba.Framework.Collections;
+
+namespace Alba.Framework.Windows.Markup
+{
+    public class DictionaryStyleSelector : StyleSelector, IDictionary
+    {
+        private readonly Dictionary<Type, Style> _typeToStyle = new Dictionary<Type, Style>();
+
+        public override Style SelectStyle (object item, DependencyObject container)
+        {
+            if (item == null)
+                return null;
+            Type itemType = item.GetType();
+            return _typeToStyle.GetOrDefault(itemType);
+        }
+
+        public void Add (object key, object value)
+        {
+            _typeToStyle.Add((Type)key, (Style)value);
+        }
+
+        bool IDictionary.Contains (object key)
+        {
+            throw BuckYouMicrosoft;
+        }
+
+        void IDictionary.Clear ()
+        {
+            throw BuckYouMicrosoft;
+        }
+
+        IDictionaryEnumerator IDictionary.GetEnumerator ()
+        {
+            throw BuckYouMicrosoft;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator ()
+        {
+            throw BuckYouMicrosoft;
+        }
+
+        void IDictionary.Remove (object key)
+        {
+            throw BuckYouMicrosoft;
+        }
+
+        object IDictionary.this [object key]
+        {
+            get { throw BuckYouMicrosoft; }
+            set { throw BuckYouMicrosoft; }
+        }
+
+        ICollection IDictionary.Keys
+        {
+            get { throw BuckYouMicrosoft; }
+        }
+
+        ICollection IDictionary.Values
+        {
+            get { throw BuckYouMicrosoft; }
+        }
+
+        bool IDictionary.IsReadOnly
+        {
+            get { throw BuckYouMicrosoft; }
+        }
+
+        bool IDictionary.IsFixedSize
+        {
+            get { throw BuckYouMicrosoft; }
+        }
+
+        void ICollection.CopyTo (Array array, int index)
+        {
+            throw BuckYouMicrosoft;
+        }
+
+        int ICollection.Count
+        {
+            get { throw BuckYouMicrosoft; }
+        }
+
+        object ICollection.SyncRoot
+        {
+            get { throw BuckYouMicrosoft; }
+        }
+
+        bool ICollection.IsSynchronized
+        {
+            get { throw BuckYouMicrosoft; }
+        }
+
+        private static Exception BuckYouMicrosoft
+        {
+            get { return new NotSupportedException("Buck you Microsoft for making StyleSelector a class instead of an interface."); }
+        }
+    }
+}
---
+++ Alba.Framework.Presentation/Windows/Mvvm/Models/LazyExpand(T).cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Alba.Framework.Attributes;
+
+namespace Alba.Framework.Windows.Mvvm
+{
+    public class LazyExpand<T> : INotifyPropertyChanged
+    {
+        private static readonly IEnumerable<T> Empty = new[] { default(T) };
+
+        private Func<IEnumerable<T>> _getItems;
+        private IEnumerable<T> _items = Empty;
+        private bool _isExpanded;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LazyExpand (Func<IEnumerable<T>> getItems)
+        {
+            if (getItems == null)
+                throw new ArgumentNullException("getItems");
+            _getItems = getItems;
+        }
+
+        public bool IsEnumerated
+        {
+            get { return _getItems == null; }
+        }
+
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                if (_isExpanded == value)
+                    return;
+                if (value)
+                    EnsureEnumerated();
+                _isExpanded = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public IEnumerable<T> Items
+        {
+            get { return _items; }
+        }
+
+        public void EnsureEnumerated ()
+        {
+            if (_getItems == null)
+                return;
+            _items = _getItems();
+            _getItems = null;
+            OnPropertyChanged("IsEnumerated");
+            OnPropertyChanged("Items");
+        }
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged ([CallerMemberName] string propName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
