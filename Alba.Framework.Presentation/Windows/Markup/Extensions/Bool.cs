b20cccb14dbdeb2086452aadba2b2ab2a29adbf7
Athari <athari.p@gmail.com>
2014-01-06 19:00:23 +0400

    Added Bool[Extension] markup extension.

---
+++ Alba.Framework.Presentation/Windows/Markup/Extensions/Bool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Alba.Framework.Windows.Markup
+{
+    /// <summary></summary>
+    /// <remarks>Named Bool and not BoolExtension to make access to constants cleaner.</remarks>
+    public class Bool : MarkupExtension
+    {
+        public const bool True = true;
+        public const bool False = false;
+
+        public bool Value { get; private set; }
+
+        public Bool (bool value)
+        {
+            Value = value;
+        }
+
+        public override object ProvideValue (IServiceProvider provider)
+        {
+            return Value;
+        }
+    }
+}
