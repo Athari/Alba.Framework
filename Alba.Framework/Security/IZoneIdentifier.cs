bbb87beb4678429472e2f2f409da9d1b37d25a0f
Athari <athari.p@gmail.com>
2014-01-27 21:36:48 +0400

    Added UrlZones for checking URL zones (MOTW).

---
+++ Alba.Framework/Security/IZoneIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Alba.Framework.Security
+{
+    [ComImport, Guid ("CD45F185-1B21-48E2-967B-EAD743A8914E"), InterfaceType (ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IZoneIdentifier
+    {
+        UrlZone GetId ();
+        void SetId (UrlZone zone);
+        void Remove ();
+    }
+}
---
+++ Alba.Framework/Security/PersistentZoneIdentifier.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Alba.Framework.Security
+{
+    [ComImport, Guid ("0968E258-16C7-4DBA-AA86-462DD61E31A3")]
+    internal class PersistentZoneIdentifier
+    {}
+}
