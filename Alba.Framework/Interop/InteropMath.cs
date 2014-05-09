d66315c5b5ef547dd1a9bd41482ae7dd385d9864
Athari <athari.p@gmail.com>
2014-05-09 21:34:36 +0400

    Added InteropMath (HighWord, LowWord).

---
+++ Alba.Framework/Interop/InteropMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Alba.Framework.Interop
+{
+    public static class InteropMath
+    {
+        public static Int16 LowWord (Int32 value)
+        {
+            return (Int16)(value & 0xffff);
+        }
+
+        public static Int16 HighWord (Int32 value)
+        {
+            return (Int16)(value >> 16);
+        }
+
+        public static Int16 LowWord (IntPtr ptr)
+        {
+            return LowWord((int)ptr);
+        }
+
+        public static Int16 HighWord (IntPtr ptr)
+        {
+            return HighWord((int)ptr);
+        }
+    }
+}
