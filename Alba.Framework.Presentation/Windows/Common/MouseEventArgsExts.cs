7ae036e550fc91899a389483a683bb481133a29c
Athari <athari.p@gmail.com>
2014-05-30 08:26:51 +0400

    Added MouseEventArgsExts.GetSourcePosition.

---
+++ Alba.Framework.Presentation/Windows/Common/MouseEventArgsExts.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Alba.Framework.Windows
+{
+    public static class MouseEventArgsExts
+    {
+        public static Point GetSourcePosition (this MouseEventArgs @this)
+        {
+            return @this.GetPosition((IInputElement)@this.Source);
+        }
+    }
+}
