214fe9d653ca392903a9113c01773204bbbeb8ca
Athari <athari.p@gmail.com>
2014-06-11 04:26:28 +0400

    Added RadioBinding markup extension for binding RadioButton to changeable values.

---
+++ Alba.Framework.Presentation/Windows/Markup/Extensions/RadioBinding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Alba.Framework.Windows.Markup
+{
+    public class RadioBinding : System.Windows.Data.MultiBinding
+    {
+        public RadioBinding (BindingBase selectedBinding, Binding valueBinding)
+        {
+            if (valueBinding.Mode == BindingMode.Default)
+                valueBinding.Mode = BindingMode.OneWay;
+            Bindings.Add(selectedBinding);
+            Bindings.Add(valueBinding);
+            Converter = new RadioEqualsConverter();
+        }
+
+        private class RadioEqualsConverter : IMultiValueConverter
+        {
+            private static readonly IMultiValueConverter _converter = new EqualsConverter();
+
+            private object[] _values;
+
+            public object Convert (object[] values, Type targetType, object parameter, CultureInfo culture)
+            {
+                _values = (object[])values.Clone();
+                return _converter.Convert(values, targetType, parameter, culture);
+            }
+
+            public object[] ConvertBack (object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            {
+                var values = new object[targetTypes.Length];
+                if (value == DependencyProperty.UnsetValue || _values == null) {
+                    for (int i = 0; i < values.Length; i++)
+                        values[i] = DependencyProperty.UnsetValue;
+                }
+                else {
+                    for (int i = 0; i < values.Length; i++)
+                        values[i] = Binding.DoNothing;
+                    if ((bool)value)
+                        values[0] = _values[1];
+                }
+                return values;
+            }
+        }
+    }
+}
