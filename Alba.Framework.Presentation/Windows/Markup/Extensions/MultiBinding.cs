b28d12c03cefed5880b32d4c4d090c71c64d60f4
Athari <athari.p@gmail.com>
2014-06-11 04:24:53 +0400

    Added MultiBinding markup extension with constructor overloads for inline MultiBinding syntax.

---
+++ Alba.Framework.Presentation/Windows/Markup/Extensions/MultiBinding.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Data;
+
+namespace Alba.Framework.Windows.Markup
+{
+    public class MultiBinding : System.Windows.Data.MultiBinding
+    {
+        public MultiBinding (BindingBase b1, BindingBase b2)
+        {
+            Bindings.Add(b1);
+            Bindings.Add(b2);
+        }
+
+        public MultiBinding (BindingBase b1, BindingBase b2, BindingBase b3)
+        {
+            Bindings.Add(b1);
+            Bindings.Add(b2);
+            Bindings.Add(b3);
+        }
+    }
+}
