dfd2cc5940a40ec9ceb785ab3e501e228506c4e2
Athari <athari.p@gmail.com>
2014-06-12 03:18:53 +0400

    Added OwnedCollectionBase<T> which modifies items when they are added/removed from it.

--- Alba.Framework/Collections/Collections/OwnedCollection(T).cs
+++ Alba.Framework/Collections/Collections/OwnedCollection(T).cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using Alba.Framework.Common;
 
 namespace Alba.Framework.Collections
 {
-    public class OwnedCollection<T, TOwner> : Collection<T>
-        where T : IOwned<TOwner>
+    public class OwnedCollection<T, TOwner> : OwnedCollectionBase<T>
+        where T : class, IOwned<TOwner>
     {
         private readonly TOwner _owner;
 
@@ -19,30 +18,14 @@ namespace Alba.Framework.Collections
             _owner = owner;
         }
 
-        protected override void InsertItem (int index, T item)
+        protected override void OwnItem (T item)
         {
-            base.InsertItem(index, item);
             item.Owner = _owner;
         }
 
-        protected override void RemoveItem (int index)
+        protected override void UnownItem (T item)
         {
-            Items[index].Owner = default(TOwner);
-            base.RemoveItem(index);
-        }
-
-        protected override void SetItem (int index, T item)
-        {
-            Items[index].Owner = default(TOwner);
-            base.SetItem(index, item);
-            Items[index].Owner = _owner;
-        }
-
-        protected override void ClearItems ()
-        {
-            for (int i = 0; i < Count; i++)
-                Items[i].Owner = default(TOwner);
-            base.ClearItems();
+            item.Owner = default (TOwner);
         }
     }
 }
---
+++ Alba.Framework/Collections/Collections/OwnedCollectionBase(T).cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Alba.Framework.Collections
+{
+    public abstract class OwnedCollectionBase<T> : Collection<T>
+        where T : class
+    {
+        protected OwnedCollectionBase ()
+        {}
+
+        protected OwnedCollectionBase (IList<T> list) : base(list)
+        {}
+
+        protected abstract void OwnItem (T item);
+
+        protected abstract void UnownItem (T item);
+
+        protected override void InsertItem (int index, T item)
+        {
+            base.InsertItem(index, item);
+            OwnItem(item);
+        }
+
+        protected override void RemoveItem (int index)
+        {
+            UnownItem(Items[index]);
+            base.RemoveItem(index);
+        }
+
+        protected override void SetItem (int index, T item)
+        {
+            UnownItem(Items[index]);
+            base.SetItem(index, item);
+            OwnItem(Items[index]);
+        }
+
+        protected override void ClearItems ()
+        {
+            for (int i = 0; i < Count; i++)
+                UnownItem(Items[i]);
+            base.ClearItems();
+        }
+    }
+}
