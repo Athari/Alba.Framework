527b877cd001c39eb8b35e32ae64391d81023412
Athari <athari.p@gmail.com>
2014-06-29 19:24:14 +0400

    Added separate event=method syntax to Message.AttachMany.

--- Alba.Framework.Caliburn/Conventions/Message.cs
+++ Alba.Framework.Caliburn/Conventions/Message.cs
@@ -11,17 +11,29 @@ namespace Alba.Framework.Caliburn
     public static partial class Message
     {
         private static readonly char[] AttachSeparators = { ';' };
+        private static readonly char[] EventMethodSeparators = { '=' };
 
         private static void AttachMany_Changed (DependencyObject d, DpChangedEventArgs<string> args)
         {
             CaliburnMicro.Message.SetAttach(d, args.NewValue
                 .Split(AttachSeparators, StringSplitOptions.RemoveEmptyEntries)
                 .Where(a => !a.IsNullOrWhitespace())
-                .Select(a => "[Event {0}] = [Action {1}]".FmtInv(GetAttachMethod(a), a))
+                .Select(GetAttachMessageItem)
                 .JoinString("; "));
         }
 
-        private static string GetAttachMethod (string attach)
+        private static string GetAttachMessageItem (string action)
+        {
+            if (action.IndexOf(EventMethodSeparators[0]) != -1) {
+                string[] eventAction = action.Split(EventMethodSeparators, 2);
+                return "[Event {0}] = [Action {1}]".FmtInv(eventAction[0], eventAction[1]);
+            }
+            else {
+                return "[Event {0}] = [Action {1}]".FmtInv(GetAttachMessageEvent(action), action);
+            }
+        }
+
+        private static string GetAttachMessageEvent (string attach)
         {
             int openParen = attach.IndexOf('(');
             return openParen == -1 ? attach : attach.Remove(openParen);
