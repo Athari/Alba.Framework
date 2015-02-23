7014a0974010349b3e573ff43198c3fb8c774a0c
Athari <athari.p@gmail.com>
2015-02-23 17:24:59 +0300

    Added support for IMultiValueConverter to StringFormatConverter.

--- Alba.Framework.Presentation/Windows/Markup/Converters/StringFormatConverter.cs
+++ Alba.Framework.Presentation/Windows/Markup/Converters/StringFormatConverter.cs
@@ -1,22 +1,29 @@
 ﻿using System;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 using Alba.Framework.Text;
 
 namespace Alba.Framework.Windows.Markup
 {
     [ValueConversion (typeof(object), typeof(string))]
-    public class StringFormatConverter : IValueConverter
+    public class StringFormatConverter : IValueConverter, IMultiValueConverter
     {
         public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (ReferenceEquals(value, DependencyProperty.UnsetValue))
-                return DependencyProperty.UnsetValue;
             return ((string)parameter).Fmt(culture, value);
         }
 
         public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public object Convert (object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string)parameter).Fmt(culture, values);
+        }
+
+        public object[] ConvertBack (object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotSupportedException();
         }
