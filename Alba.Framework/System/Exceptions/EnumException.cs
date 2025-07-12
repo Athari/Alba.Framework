4fc354bca20fac67784d296ed6b8051c688b77f6
Athari <athari.p@gmail.com>
2025-07-12 08:30:39 +0300

    Added EnumException with helper methods.

---
+++ Alba.Framework/System/Exceptions/EnumException.cs
@@ -0,0 +1,27 @@
+﻿namespace Alba.Framework;
+
+public static class EnumException
+{
+    public static EnumException<T> Create<T>([InvokerParameterName] string? paramName, T actualValue, string? message = null)
+        => new(paramName, actualValue, message);
+
+    public static EnumException<T> Create<T>([InvokerParameterName] string? paramName, string? message = null)
+        => new(paramName, message);
+}
+
+[PublicAPI]
+public class EnumException<T> : ArgumentOutOfRangeException
+{
+    public EnumException() { }
+
+    public EnumException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    public EnumException([InvokerParameterName] string? paramName, T actualValue, string? message = null)
+        : base(paramName, actualValue, message ?? FormatDefaultMessage(actualValue)) { }
+
+    public EnumException([InvokerParameterName] string? paramName, string? message)
+        : base(paramName, message ?? FormatDefaultMessage()) { }
+
+    private static string FormatDefaultMessage(object? actualValue = null) =>
+        $"{actualValue ?? "Argument"} is not a valid {typeof(T).GetTypeFullName()}";
+}
