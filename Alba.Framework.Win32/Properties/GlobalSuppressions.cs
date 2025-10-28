fb274877294503ede50db6b3be96942ec0bd230a
Athari <athari.p@gmail.com>
2025-10-28 17:09:23 +0300

    Alba.Framework.Win32 fixes: * Added workaround for COM interface generation warnings.

---
+++ Alba.Framework.Win32/Properties/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+        "Usage",
+        "CA2256:All members declared in parent interfaces must have an implementation in a DynamicInterfaceCastableImplementation-attributed interface",
+        Justification = "Generated Code and analyzer ignores that it is generated AND partial. Fixed in https://github.com/dotnet/runtime/pull/114642.",
+        Scope = "type",
+        Target = "~T:InterfaceImplementation"),
+]
