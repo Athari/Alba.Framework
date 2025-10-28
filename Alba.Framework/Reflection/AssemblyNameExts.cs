8c6578b5b09ee3878bc012224028fdaa0524092f
Athari <athari.p@gmail.com>
2025-10-28 22:59:04 +0300

    Alba.Framework/Reflection: Added AssemblyNameExts with Microsoft public key and pack URI generation.

---
+++ Alba.Framework/Reflection/AssemblyNameExts.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Alba.Framework.Reflection;
+
+public static class AssemblyNameExts
+{
+    private static readonly byte[] MicrosoftPublicKeyToken = BitConverter.GetBytes(0x_354E_36AD_5638_BF31);
+
+    public static AssemblyName WithPublicKeyToken(this AssemblyName @this, byte[] token)
+    {
+        @this.SetPublicKeyToken(token);
+        return @this;
+    }
+
+    public static AssemblyName WithMicrosoftPublicKeyToken(this AssemblyName @this) =>
+        @this.WithPublicKeyToken(MicrosoftPublicKeyToken);
+
+    public static Uri ToPackUri(this AssemblyName @this, string resourcePath) =>
+        new($"/{@this};component{resourcePath}", UriKind.Relative);
+}
