ac875a3a0c93e93d2bed939117e59e5ab7564f31
Athari <athari.p@gmail.com>
2025-10-28 17:16:24 +0300

    Alba.Framework/Collections features: * Added AsyncEnumerableExts.WhereNotNull.

---
+++ Alba.Framework/Collections/Extensions/AsyncEnumerableExts.cs
@@ -0,0 +1,12 @@
+﻿namespace Alba.Framework.Collections;
+
+public static class AsyncEnumerableExts
+{
+    extension<T>(IAsyncEnumerable<T?> @this)
+    {
+        public IAsyncEnumerable<T> WhereNotNull()
+        {
+            return @this.Where(i => i != null)!;
+        }
+    }
+}
