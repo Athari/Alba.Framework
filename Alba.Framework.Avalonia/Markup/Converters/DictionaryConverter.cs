4bdaef1d7c30713eed205355648c8f0103ada2fb
Athari <athari.p@gmail.com>
2025-10-31 20:56:43 +0300

    Alba.Framework.Avalonia/Markup/Converters: Added DictionaryConverter (ported from WPF).

---
+++ Alba.Framework.Avalonia/Markup/Converters/DictionaryConverter.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using Alba.Framework.Collections;
+using Alba.Framework.Common;
+using Avalonia.Data.Converters;
+
+namespace Alba.Framework.Avalonia.Markup.Converters;
+
+public class DictionaryConverter : Map<object, object>, IValueConverter, ISupportInitialize
+{
+    public static readonly object Defaut = new NamedObject("DictionaryConverter.Default");
+
+    private object? _defaultValue;
+
+    public ICollection<object>? ValidateKeys { get; set; }
+    public ICollection<object>? ValidateValues { get; set; }
+
+    protected override void AddItem(object key, object value)
+    {
+        if (key == Defaut) {
+            _defaultValue = value;
+            return;
+        }
+        base.AddItem(key, value);
+    }
+
+    void ISupportInitialize.BeginInit() { }
+
+    void ISupportInitialize.EndInit()
+    {
+        if (ValidateKeys != null)
+            ValidateItems(ValidateKeys, Keys, "key");
+        if (ValidateValues != null)
+            ValidateItems(ValidateValues, Values, "value");
+    }
+
+    private static void ValidateItems(ICollection<object> expected, ICollection<object> actual, string propName)
+    {
+        if (expected.Count != actual.Count)
+            throw new ArgumentException($"Validation of {propName}s in DictionaryConverter failed: wrong item number (expected: {expected.Count}, actual: {actual.Count}).");
+        foreach (object value in expected)
+            if (!actual.Any(a => a.EqualsValue(value)))
+                throw new ArgumentException($"Validation of {propName}s in DictionaryConverter failed: {propName} '{value}' is missing.");
+    }
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        this.Get(value!, _defaultValue);
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotSupportedException();
+}
