2b44c4ef94d77abddc3a31bdfe1f54d3fceddafa
Athari <athari.p@gmail.com>
2025-11-07 08:34:53 +0300

    Alba.Framework: Added FileExts.TryDelete.

---
+++ Alba.Framework/IO/FileExts.cs
@@ -0,0 +1,20 @@
+﻿namespace Alba.Framework.IO;
+
+public static class FileExts
+{
+    extension(File)
+    {
+        public static bool TryDelete(string? path)
+        {
+            try {
+                if (File.Exists(path)) {
+                    File.Delete(path);
+                    return true;
+                }
+            }
+            catch { /* ignored */
+            }
+            return false;
+        }
+    }
+}
