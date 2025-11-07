292025f00df027ed1dfd9677fa3e81f76f78570a
Athari <athari.p@gmail.com>
2025-11-07 08:35:26 +0300

    Alba.Framework.Avalonia/Markup: Added NotEqualsConverter.

---
+++ Alba.Framework.Avalonia/Markup/Converters/NotEqualsConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Markup.Xaml;
+
+namespace Alba.Framework.Avalonia.Markup.Converters;
+
+public class NotEqualsConverter : MarkupExtension, IValueConverter
+{
+    public object? Value { get; set; }
+
+    public NotEqualsConverter() { }
+
+    public NotEqualsConverter(object value) =>
+        Value = value;
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        !Equals(value, Value);
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        !Equals(value, true) ? Value : AvaloniaProperty.UnsetValue;
+
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+        this;
+}
