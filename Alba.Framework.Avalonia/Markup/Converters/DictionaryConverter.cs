87f8a765ec6f5de97270add98fcaebaf1b5411c2
Athari <athari.p@gmail.com>
2025-11-12 05:56:55 +0300

    Alba.Framework.Avalonia/Markup: Fixed nullability in DictionaryConverter (allow values to be null, enforce non-null keys).

--- Alba.Framework.Avalonia/Markup/Converters/DictionaryConverter.cs
+++ Alba.Framework.Avalonia/Markup/Converters/DictionaryConverter.cs
@@ -6,16 +6,16 @@ using Avalonia.Data.Converters;
 
 namespace Alba.Framework.Avalonia.Markup.Converters;
 
-public class DictionaryConverter : Map<object, object>, IValueConverter, ISupportInitialize
+public class DictionaryConverter : Map<object, object?>, IValueConverter, ISupportInitialize
 {
     public static readonly object Defaut = new NamedObject("DictionaryConverter.Default");
 
     private object? _defaultValue;
 
     public ICollection<object>? ValidateKeys { get; set; }
-    public ICollection<object>? ValidateValues { get; set; }
+    public ICollection<object?>? ValidateValues { get; set; }
 
-    protected override void AddItem(object key, object value)
+    protected override void AddItem(object key, object? value)
     {
         if (key == Defaut) {
             _defaultValue = value;
@@ -34,17 +34,17 @@ public class DictionaryConverter : Map<object, object>, IValueConverter, ISuppor
             ValidateItems(ValidateValues, Values, "value");
     }
 
-    private static void ValidateItems(ICollection<object> expected, ICollection<object> actual, string propName)
+    private static void ValidateItems<T>(ICollection<T> expected, ICollection<T> actual, string propName)
     {
         if (expected.Count != actual.Count)
             throw new ArgumentException($"Validation of {propName}s in DictionaryConverter failed: wrong item number (expected: {expected.Count}, actual: {actual.Count}).");
-        foreach (object value in expected)
+        foreach (var value in expected)
             if (!actual.Any(a => a.EqualsValue(value)))
                 throw new ArgumentException($"Validation of {propName}s in DictionaryConverter failed: {propName} '{value}' is missing.");
     }
 
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
-        this.Get(value!, _defaultValue);
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        this.Get(Ensure.NotNull(value), _defaultValue);
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
         throw new NotSupportedException();
