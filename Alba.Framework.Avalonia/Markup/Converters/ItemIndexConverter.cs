f094a1280d01794efb1adef54f675e83fc3f4f22
Athari <athari.p@gmail.com>
2025-11-12 13:41:52 +0300

    Alba.Framework.Avalonia/Markup: Added ItemIndexConverter.

---
+++ Alba.Framework.Avalonia/Markup/Converters/ItemIndexConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Globalization;
+
+namespace Alba.Framework.Avalonia.Markup.Converters;
+
+[PublicAPI]
+public class ItemIndexConverter : MultiValueConverterBase<object?, IList?, int?>
+{
+    public int Offset { get; set; } = 1;
+    public int? Default { get; set; }
+
+    public override int? Convert(object? item, IList? list, Type targetType, CultureInfo culture)
+    {
+        var index = list?.IndexOf(item);
+        return index is null or -1 ? Default : index + Offset;
+    }
+}
