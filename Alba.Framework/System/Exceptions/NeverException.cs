a4b0240438a9b4bb9b9df6d8b0ed12a7d4020cb9
Athari <athari.p@gmail.com>
2025-11-12 06:04:28 +0300

    Alba.Framework: Added NeverException for never-happening switch cases etc.

---
+++ Alba.Framework/System/Exceptions/NeverException.cs
@@ -0,0 +1,10 @@
+﻿namespace Alba.Framework;
+
+public class NeverException : InvalidOperationException
+{
+    private const string DefaultMessage = "Internal logic error. This error should have never happened.";
+
+    public NeverException() : base(DefaultMessage) { }
+    public NeverException(string message) : base(message) { }
+    public NeverException(string message, Exception inner) : base(message, inner) { }
+}
