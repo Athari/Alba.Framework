972621012cbe79932945b06740bab6949b3a908a
Athari <athari.p@gmail.com>
2025-11-18 11:55:42 +0300

    Alba.Framework/Threading: Added methods to NotifyPropertyChangedChannelExts with manual propperty list instead of expressions.

--- Alba.Framework/Threading/Channels/NotifyPropertyChangedChannelExts.cs
+++ Alba.Framework/Threading/Channels/NotifyPropertyChangedChannelExts.cs
@@ -9,61 +9,6 @@ namespace Alba.Framework.Threading.Channels;
 [PublicAPI]
 public static class NotifyPropertyChangedChannelExts
 {
-    extension(INotifyPropertyChanged @this)
-    {
-        public ChannelReader<T> PropertyChangedToBoundedChannel<T>(
-            LambdaExpression prop, Func<T> getValue,
-            BoundedChannelOptions options, CancellationToken ct)
-        {
-            options.SingleWriter = true;
-            var channel = Channel.CreateBounded<T>(options);
-            return @this.SetupChannel(prop, getValue, channel, ct);
-        }
-
-        public ChannelReader<T> PropertyChangedToUnboundedChannel<T>(
-            LambdaExpression prop, Func<T> getValue,
-            UnboundedChannelOptions? options, CancellationToken ct)
-        {
-            options ??= new();
-            options.SingleWriter = true;
-            var channel = Channel.CreateUnbounded<T>(options);
-            return @this.SetupChannel(prop, getValue, channel, ct);
-        }
-
-        public ChannelReader<T> PropertyChangedToUnboundedPrioritizedChannel<T>(
-            LambdaExpression prop, Func<T> getValue,
-            UnboundedPrioritizedChannelOptions<T>? options, CancellationToken ct)
-        {
-            options ??= new();
-            options.SingleWriter = true;
-            var channel = Channel.CreateUnboundedPrioritized(options);
-            return @this.SetupChannel(prop, getValue, channel, ct);
-        }
-
-        private ChannelReader<T> SetupChannel<T>(
-            LambdaExpression prop, Func<T> getValue,
-            Channel<T> channel, CancellationToken ct)
-        {
-            Guard.IsNotDefault(ct);
-
-            var propertyName = PropertyName.Of(prop);
-            @this.PropertyChanged += OnPropertyChanged;
-            ct.Register(() => {
-                @this.PropertyChanged -= OnPropertyChanged;
-                channel.Writer.Complete();
-            });
-            channel.Reader.Completion.ContinueWith(_ =>
-                @this.PropertyChanged -= OnPropertyChanged, ct);
-            return channel.Reader;
-
-            void OnPropertyChanged(object? _, PropertyChangedEventArgs e)
-            {
-                if (e.PropertyName == null || e.PropertyName == propertyName)
-                    channel.Writer.WriteAsync(getValue(), ct).NoAwait();
-            }
-        }
-    }
-
     extension<TObj>(TObj @this) where TObj : INotifyPropertyChanged
     {
         public ChannelReader<T> PropertyChangedToBoundedChannel<T>(
@@ -88,6 +33,96 @@ public static class NotifyPropertyChangedChannelExts
         {
             var getter = prop.Compile();
             return @this.PropertyChangedToUnboundedPrioritizedChannel(prop, () => getter(@this), options, ct);
+        }
+    }
+
+    extension(INotifyPropertyChanged @this)
+    {
+        public ChannelReader<T> PropertyChangedToBoundedChannel<T>(
+            string[] propertyNames, Func<T> getValue,
+            BoundedChannelOptions options, CancellationToken ct)
+        {
+            options.SingleWriter = true;
+            var channel = Channel.CreateBounded<T>(options);
+            return @this.SetupChannel(propertyNames, getValue, channel, ct);
+        }
+
+        public ChannelReader<T> PropertyChangedToUnboundedChannel<T>(
+            string[] propertyNames, Func<T> getValue,
+            UnboundedChannelOptions? options, CancellationToken ct)
+        {
+            options ??= new();
+            options.SingleWriter = true;
+            var channel = Channel.CreateUnbounded<T>(options);
+            return @this.SetupChannel(propertyNames, getValue, channel, ct);
+        }
+
+        public ChannelReader<T> PropertyChangedToUnboundedPrioritizedChannel<T>(
+            string[] propertyNames, Func<T> getValue,
+            UnboundedPrioritizedChannelOptions<T>? options, CancellationToken ct)
+        {
+            options ??= new();
+            options.SingleWriter = true;
+            var channel = Channel.CreateUnboundedPrioritized(options);
+            return @this.SetupChannel(propertyNames, getValue, channel, ct);
+        }
+
+        private ChannelReader<T> PropertyChangedToBoundedChannel<T>(
+            LambdaExpression prop, Func<T> getValue,
+            BoundedChannelOptions options, CancellationToken ct)
+        {
+            options.SingleWriter = true;
+            var channel = Channel.CreateBounded<T>(options);
+            return @this.SetupChannel(prop, getValue, channel, ct);
+        }
+
+        private ChannelReader<T> PropertyChangedToUnboundedChannel<T>(
+            LambdaExpression prop, Func<T> getValue,
+            UnboundedChannelOptions? options, CancellationToken ct)
+        {
+            options ??= new();
+            options.SingleWriter = true;
+            var channel = Channel.CreateUnbounded<T>(options);
+            return @this.SetupChannel(prop, getValue, channel, ct);
+        }
+
+        private ChannelReader<T> PropertyChangedToUnboundedPrioritizedChannel<T>(
+            LambdaExpression prop, Func<T> getValue,
+            UnboundedPrioritizedChannelOptions<T>? options, CancellationToken ct)
+        {
+            options ??= new();
+            options.SingleWriter = true;
+            var channel = Channel.CreateUnboundedPrioritized(options);
+            return @this.SetupChannel(prop, getValue, channel, ct);
+        }
+
+        private ChannelReader<T> SetupChannel<T>(
+            LambdaExpression prop, Func<T> getValue,
+            Channel<T> channel, CancellationToken ct)
+        {
+            return @this.SetupChannel([ PropertyName.Of(prop) ], getValue, channel, ct);
+        }
+
+        private ChannelReader<T> SetupChannel<T>(
+            string[] propertyNames, Func<T> getValue,
+            Channel<T> channel, CancellationToken ct)
+        {
+            Guard.IsNotDefault(ct);
+
+            @this.PropertyChanged += OnPropertyChanged;
+            ct.Register(() => {
+                @this.PropertyChanged -= OnPropertyChanged;
+                channel.Writer.Complete();
+            });
+            channel.Reader.Completion.ContinueWith(_ =>
+                @this.PropertyChanged -= OnPropertyChanged, ct);
+            return channel.Reader;
+
+            void OnPropertyChanged(object? _, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == null || propertyNames.Contains(e.PropertyName))
+                    channel.Writer.WriteAsync(getValue(), ct).NoWait();
+            }
         }
     }
 }
