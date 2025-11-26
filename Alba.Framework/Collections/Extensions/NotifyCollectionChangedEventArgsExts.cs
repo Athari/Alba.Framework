c08f9a1f097336354bb0a31571656b80f4fc5f68
Athari <athari.p@gmail.com>
2025-11-27 01:17:56 +0300

    Alba.Framework/Collections: Added NotifyCollectionChangedEventArgsExts.GetNewItems/GetOldItems.

---
+++ Alba.Framework/Collections/Extensions/NotifyCollectionChangedEventArgsExts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Specialized;
+
+namespace Alba.Framework.Collections;
+
+public static class NotifyCollectionChangedEventArgsExts
+{
+    extension(NotifyCollectionChangedEventArgs @this)
+    {
+        public IEnumerable<T> GetNewItems<T>() => @this.NewItems?.Cast<T>() ?? [ ];
+        public IEnumerable<T> GetOldItems<T>() => @this.OldItems?.Cast<T>() ?? [ ];
+    }
+}
