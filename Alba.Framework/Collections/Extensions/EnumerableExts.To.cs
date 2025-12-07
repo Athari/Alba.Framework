6102b3b6d15362ae5e5ca1d011b23c9066c01d32
Athari <athari.p@gmail.com>
2025-12-07 10:38:56 +0300

    Alba.Framework/Collections: Added EnumerableExts.To[ReadOnly]DictionarySafe * Fixed ToReadOnlyDictionary to accept element selector changing type * Changed overloads with comparer to accept null

--- Alba.Framework/Collections/Extensions/EnumerableExts.To.cs
+++ Alba.Framework/Collections/Extensions/EnumerableExts.To.cs
@@ -25,46 +25,100 @@ public static partial class EnumerableExts
         public HashSet<TKey> ToHashSet<TKey>(Func<T, TKey> keySelector) =>
             @this.Select(keySelector).ToHashSet();
 
-        public HashSet<TKey> ToHashSet<TKey>(Func<T, TKey> keySelector, IEqualityComparer<TKey> comparer) =>
+        public HashSet<TKey> ToHashSet<TKey>(Func<T, TKey> keySelector, IEqualityComparer<TKey>? comparer) =>
             @this.Select(keySelector).ToHashSet(comparer);
 
         public SortedSet<T> ToSortedSet() =>
             new(@this);
 
-        public SortedSet<T> ToSortedSet(IComparer<T> comparer) =>
+        public SortedSet<T> ToSortedSet(IComparer<T>? comparer) =>
             new(@this, comparer);
 
         public SortedSet<TKey> ToSortedSet<TKey>(Func<T, TKey> keySelector) =>
             new(@this.Select(keySelector));
 
-        public SortedSet<TKey> ToSortedSet<TKey>(Func<T, TKey> keySelector, IComparer<TKey> comparer) =>
+        public SortedSet<TKey> ToSortedSet<TKey>(Func<T, TKey> keySelector, IComparer<TKey>? comparer) =>
             new(@this.Select(keySelector), comparer);
+
+        public Dictionary<TKey, T> ToDictionarySafe<TKey>(Func<T, TKey> keySelector)
+            where TKey : notnull =>
+            @this.ToDictionarySafe(keySelector, null);
+
+        public Dictionary<TKey, T> ToDictionarySafe<TKey>(Func<T, TKey> keySelector, IEqualityComparer<TKey>? comparer)
+            where TKey : notnull
+        {
+            var dic = new Dictionary<TKey, T>(comparer);
+            foreach (var item in @this)
+                dic[keySelector(item)] = item;
+            return dic;
+        }
+
+        public Dictionary<TKey, TValue> ToDictionarySafe<TKey, TValue>(Func<T, TKey> keySelector, Func<T, TValue> elementSelector)
+            where TKey : notnull =>
+            @this.ToDictionarySafe(keySelector, elementSelector, null);
+
+        public Dictionary<TKey, TValue> ToDictionarySafe<TKey, TValue>(Func<T, TKey> keySelector, Func<T, TValue> elementSelector, IEqualityComparer<TKey>? comparer)
+            where TKey : notnull
+        {
+            var dic = new Dictionary<TKey, TValue>(comparer);
+            foreach (var item in @this)
+                dic[keySelector(item)] = elementSelector(item);
+            return dic;
+        }
 
         public ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey>(Func<T, TKey> keySelector)
             where TKey : notnull =>
             new(@this.ToDictionary(keySelector));
 
-        public ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey>(Func<T, TKey> keySelector, IEqualityComparer<TKey> comparer)
+        public ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey>(Func<T, TKey> keySelector, IEqualityComparer<TKey>? comparer)
             where TKey : notnull =>
             new(@this.ToDictionary(keySelector, comparer));
 
-        public ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey>(Func<T, TKey> keySelector, Func<T, T> elementSelector)
+        public ReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<TKey, TValue>(Func<T, TKey> keySelector, Func<T, TValue> elementSelector)
             where TKey : notnull =>
             new(@this.ToDictionary(keySelector, elementSelector));
 
-        public ReadOnlyDictionary<TKey, T> ToReadOnlyDictionary<TKey>(Func<T, TKey> keySelector, Func<T, T> elementSelector, IEqualityComparer<TKey> comparer)
+        public ReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<TKey, TValue>(Func<T, TKey> keySelector, Func<T, TValue> elementSelector, IEqualityComparer<TKey>? comparer)
             where TKey : notnull =>
             new(@this.ToDictionary(keySelector, elementSelector, comparer));
+
+        public ReadOnlyDictionary<TKey, T> ToReadOnlyDictionarySafe<TKey>(Func<T, TKey> keySelector)
+            where TKey : notnull =>
+            new(@this.ToDictionarySafe(keySelector));
+
+        public ReadOnlyDictionary<TKey, T> ToReadOnlyDictionarySafe<TKey>(Func<T, TKey> keySelector, IEqualityComparer<TKey>? comparer)
+            where TKey : notnull =>
+            new(@this.ToDictionarySafe(keySelector, comparer));
+
+        public ReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionarySafe<TKey, TValue>(Func<T, TKey> keySelector, Func<T, TValue> elementSelector)
+            where TKey : notnull =>
+            new(@this.ToDictionarySafe(keySelector, elementSelector));
+
+        public ReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionarySafe<TKey, TValue>(Func<T, TKey> keySelector, Func<T, TValue> elementSelector, IEqualityComparer<TKey>? comparer)
+            where TKey : notnull =>
+            new(@this.ToDictionarySafe(keySelector, elementSelector, comparer));
 
         public ReadOnlyCollection<T> ToReadOnlyList() =>
             @this.ToList().AsReadOnly();
     }
 
-    public static Dictionary<TKey, T> ToDictionary<T, TKey>(this IEnumerable<KeyValuePair<TKey, T>> @this)
-        where TKey : notnull =>
-        @this.ToDictionary(p => p.Key, p => p.Value);
+    extension<T, TKey>(IEnumerable<KeyValuePair<TKey, T>> @this) where TKey : notnull
+    {
+        public Dictionary<TKey, T> ToDictionary() =>
+            @this.ToDictionary(p => p.Key, p => p.Value);
 
-    public static Dictionary<TKey, T> ToDictionary<T, TKey>(this IEnumerable<DictionaryEntry> @this)
-        where TKey : notnull =>
-        @this.ToDictionary(p => (TKey)p.Key, p => (T)p.Value!);
+        public Dictionary<TKey, T> ToDictionarySafe() =>
+            @this.ToDictionarySafe(p => p.Key, p => p.Value);
+    }
+
+    extension(IEnumerable<DictionaryEntry> @this)
+    {
+        public Dictionary<TKey, T> ToDictionary<T, TKey>()
+            where TKey : notnull =>
+            @this.ToDictionary(p => (TKey)p.Key, p => (T)p.Value!);
+
+        public Dictionary<TKey, T> ToDictionarySafe<T, TKey>()
+            where TKey : notnull =>
+            @this.ToDictionarySafe(p => (TKey)p.Key, p => (T)p.Value!);
+    }
 }
