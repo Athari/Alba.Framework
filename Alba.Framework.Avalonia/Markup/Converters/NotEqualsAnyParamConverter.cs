d55d47f1065c3bb9c9b2e42a90cc008cf141df4d
Athari <athari.p@gmail.com>
2025-12-14 20:17:47 +0300

    Alba.Framework.Avalonia/Markup: Added NotEqualsAnyParamConverter.

---
+++ Alba.Framework.Avalonia/Markup/Converters/NotEqualsAnyParamConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace Alba.Framework.Avalonia.Markup.Converters;
+
+public class NotEqualsAnyParamConverter : ValueConverterBase<object?, bool, IEnumerable<object?>?>
+{
+    public override bool Convert(object? value, Type targetType, IEnumerable<object?>? parameter, CultureInfo culture) =>
+        !(parameter?.Any(p => Equals(value, p)) ?? false);
+}
