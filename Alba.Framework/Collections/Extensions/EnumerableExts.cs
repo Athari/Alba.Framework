343989328398e799e95a3b5d55b112e207b0f705
Athari <athari.p@gmail.com>
2026-01-07 07:07:22 +0300

    Alba.Framework/Collections: Added struct methods to EnumerableExts: FirstOrNull, LastOrNull, AtOrNull.

--- Alba.Framework/Collections/Extensions/EnumerableExts.cs
+++ Alba.Framework/Collections/Extensions/EnumerableExts.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Alba.Framework.Collections;
 
@@ -265,6 +267,172 @@ public static partial class EnumerableExts
         }
     }
 
+    extension<T>(IEnumerable<T> @this) where T : struct
+    {
+        public T? FirstOrNull() =>
+            @this.TryGetFirst(out _);
+
+        public T? FirstOrNull(Func<T, bool> predicate) =>
+            @this.TryGetFirst(predicate, out _);
+
+        public T? LastOrNull() =>
+            @this.TryGetLast(out _);
+
+        public T? LastOrNull(Func<T, bool> predicate) =>
+            @this.TryGetLast(predicate, out _);
+
+        public T? AtOrNull(int index) =>
+            @this.TryGetElementAt(index, out _);
+
+        public T? AtOrNull(Index index)
+        {
+            if (!index.IsFromEnd)
+                return @this.AtOrNull(index.Value);
+            if (@this.TryGetNonEnumeratedCount(out int count))
+                return @this.AtOrNull(count - index.Value);
+            TryGetElementFromEnd(@this, index.Value, out var item);
+            return item;
+        }
+
+        private T? TryGetFirst(out bool found)
+        {
+            if (@this is IList<T> list) {
+                if (list.Count > 0) {
+                    found = true;
+                    return list[0];
+                }
+            }
+            else {
+                using var it = @this.GetEnumerator();
+                if (it.MoveNext()) {
+                    found = true;
+                    return it.Current;
+                }
+            }
+            found = false;
+            return null;
+        }
+
+        private T? TryGetFirst(Func<T, bool> predicate, out bool found)
+        {
+            if (@this.TryGetSpan(out ReadOnlySpan<T> span)) {
+                foreach (var item in span) {
+                    if (predicate(item)) {
+                        found = true;
+                        return item;
+                    }
+                }
+            }
+            else {
+                foreach (var item in @this) {
+                    if (predicate(item)) {
+                        found = true;
+                        return item;
+                    }
+                }
+            }
+            found = false;
+            return null;
+        }
+
+        private T? TryGetLast(out bool found)
+        {
+            if (@this is IList<T> list) {
+                var count = list.Count;
+                if (count > 0) {
+                    found = true;
+                    return list[count - 1];
+                }
+            }
+            else {
+                using var it = @this.GetEnumerator();
+                if (it.MoveNext()) {
+                    T result;
+                    do {
+                        result = it.Current;
+                    } while (it.MoveNext());
+                    found = true;
+                    return result;
+                }
+            }
+            found = false;
+            return null;
+        }
+
+        private T? TryGetLast(Func<T, bool> predicate, out bool found)
+        {
+            if (@this is IList<T> list) {
+                for (var i = list.Count - 1; i >= 0; --i) {
+                    var result = list[i];
+                    if (predicate(result)) {
+                        found = true;
+                        return result;
+                    }
+                }
+            }
+            else {
+                using var it = @this.GetEnumerator();
+                while (it.MoveNext()) {
+                    var result = it.Current;
+                    if (predicate(result)) {
+                        while (it.MoveNext()) {
+                            var item = it.Current;
+                            if (predicate(item))
+                                result = item;
+                        }
+                        found = true;
+                        return result;
+                    }
+                }
+            }
+
+            found = false;
+            return null;
+        }
+
+        private T? TryGetElementAt(int index, out bool found)
+        {
+            if (@this is IList<T> list) {
+                found = (uint)index < (uint)list.Count;
+                return found ? list[index] : null;
+            }
+            if (index >= 0) {
+                using var it = @this.GetEnumerator();
+                while (it.MoveNext()) {
+                    if (index == 0) {
+                        found = true;
+                        return it.Current;
+                    }
+                    index--;
+                }
+            }
+            found = false;
+            return null;
+        }
+
+        private bool TryGetElementFromEnd(int indexFromEnd, [MaybeNullWhen(false)] out T? item)
+        {
+            if (indexFromEnd > 0) {
+                using IEnumerator<T> e = @this.GetEnumerator();
+                if (e.MoveNext()) {
+                    Queue<T> queue = new();
+                    queue.Enqueue(e.Current);
+                    while (e.MoveNext()) {
+                        if (queue.Count == indexFromEnd)
+                            queue.Dequeue();
+                        queue.Enqueue(e.Current);
+                    }
+                    if (queue.Count == indexFromEnd) {
+                        item = queue.Dequeue();
+                        return true;
+                    }
+                }
+            }
+            item = null;
+            return false;
+        }
+    }
+
     public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> @this)
     {
         return @this.SelectMany(i => i);
@@ -292,6 +460,23 @@ public static partial class EnumerableExts
 
         public IEnumerable<int> Range(int count) =>
             Enumerable.Range(@this, count);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static bool TryGetSpan<T>([NoEnumeration] this IEnumerable<T> @this, out ReadOnlySpan<T> span)
+    {
+        if (@this.GetType() == typeof(T[])) {
+            span = Unsafe.As<T[]>(@this);
+            return true;
+        }
+        else if (@this.GetType() == typeof(List<T>)) {
+            span = CollectionsMarshal.AsSpan(Unsafe.As<List<T>>(@this));
+            return true;
+        }
+        else {
+            span = default;
+            return false;
+        }
     }
 
     private static class EmptyEnumerable<T>
