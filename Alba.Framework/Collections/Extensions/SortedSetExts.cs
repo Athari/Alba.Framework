01572105ab8119006e2c8f6e18656fed473c09ca
Athari <athari.p@gmail.com>
2026-01-07 12:52:30 +0300

    Alba.Framework/Collections: Added SortedSetExts with methods: GetOrDefault, GetOrNull.

---
+++ Alba.Framework/Collections/Extensions/SortedSetExts.cs
@@ -0,0 +1,16 @@
+﻿namespace Alba.Framework.Collections;
+
+public static class SortedSetExts
+{
+    extension<T>(SortedSet<T> @this)
+    {
+        public T? GetOrDefault(T value) =>
+            @this.TryGetValue(value, out var v) ? v : default;
+    }
+
+    extension<T>(SortedSet<T> @this) where T : struct
+    {
+        public T? GetOrNull(T value) =>
+            @this.TryGetValue(value, out var v) ? v : null;
+    }
+}
