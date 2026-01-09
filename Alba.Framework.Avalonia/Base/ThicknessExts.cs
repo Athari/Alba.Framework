b9fbc83c04a9829c05faa196da2e487fba78c456
Athari <athari.p@gmail.com>
2026-01-09 09:14:59 +0300

    Alba.Framework.Avalonia: Added ThicknessExts with IsZero property.

---
+++ Alba.Framework.Avalonia/Base/ThicknessExts.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+
+namespace Alba.Framework.Avalonia;
+
+public static class ThicknessExts
+{
+    extension(Thickness @this)
+    {
+        public bool IsZero => @this == new Thickness();
+    }
+}
