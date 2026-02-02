32144ec5a361527e4a0da5e490f8f80334cc27ff
Athari <athari.p@gmail.com>
2026-02-03 01:48:21 +0300

    Alba.Framework.Avalonia: Added PixelSizeExts, PixelThickness.

---
+++ Alba.Framework.Avalonia/Base/PixelSizeExts.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+
+namespace Alba.Framework.Avalonia;
+
+public static class PixelSizeExts
+{
+    extension(PixelSize @this)
+    {
+        public bool IsZero => @this == new PixelSize();
+
+        public static PixelSize Min(PixelSize a, PixelSize b) =>
+            Extremum(a, b, Math.Min);
+
+        public static PixelSize Max(PixelSize a, PixelSize b) =>
+            Extremum(a, b, Math.Max);
+
+        private static PixelSize Extremum(PixelSize a, PixelSize b, Func<int, int, int> f) =>
+            new(f(a.Width, b.Width), f(a.Height, b.Height));
+    }
+}
---
+++ Alba.Framework.Avalonia/Base/PixelThickness.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+
+namespace Alba.Framework.Avalonia;
+
+public record struct PixelThickness(int left, int top, int right, int bottom)
+{
+    public readonly int Left = left;
+    public readonly int Top = top;
+    public readonly int Right = right;
+    public readonly int Bottom = bottom;
+
+    public PixelThickness(int horizontal, int vertical) : this(horizontal, vertical, horizontal, vertical) { }
+
+    public PixelThickness(int uniform) : this(uniform, uniform, uniform, uniform) { }
+
+    public PixelThickness() : this(0, 0, 0, 0) { }
+
+    public bool IsZero => this == new PixelThickness();
+
+    public static PixelThickness Min(PixelThickness a, PixelThickness b) =>
+        Extremum(a, b, Math.Min);
+
+    public static PixelThickness Max(PixelThickness a, PixelThickness b) =>
+        Extremum(a, b, Math.Max);
+
+    private static PixelThickness Extremum(PixelThickness a, PixelThickness b, Func<int, int, int> f) =>
+        new(f(a.Left, b.Left), f(a.Top, b.Top), f(a.Right, b.Right), f(a.Bottom, b.Bottom));
+
+    public Thickness ToFloat() => new(Left, Top, Right, Bottom);
+}
