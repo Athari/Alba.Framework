72e769dfae6d77b658708e8f803c8e8804cf6c5b
Athari <athari.p@gmail.com>
2026-02-03 01:50:13 +0300

    Alba.Framework/Globalization: Added CultureScope which overrides CultureInfo.CurrentCulture of current context while it exists.

---
+++ Alba.Framework/Globalization/CultureScope.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Alba.Framework.Globalization;
+
+public sealed class CultureScope : IDisposable
+{
+    private readonly CultureInfo _oldCulture;
+
+    public CultureScope(CultureInfo? culture = null)
+    {
+        _oldCulture = CultureInfo.CurrentCulture;
+        CultureInfo.CurrentCulture = culture ?? CultureInfo.InvariantCulture;
+    }
+
+    public void Dispose()
+    {
+        CultureInfo.CurrentCulture = _oldCulture;
+    }
+}
