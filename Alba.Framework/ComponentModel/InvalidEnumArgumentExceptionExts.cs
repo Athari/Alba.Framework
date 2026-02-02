940617d37c2f692825fe4191ca151d9d721f9374
Athari <athari.p@gmail.com>
2026-02-03 01:49:16 +0300

    Alba.Framework/ComponentModel: Added InvalidEnumArgumentExceptionExts with methods Create and Throw which generate message without param name.

---
+++ Alba.Framework/ComponentModel/InvalidEnumArgumentExceptionExts.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Alba.Framework.Reflection;
+
+namespace Alba.Framework.ComponentModel;
+
+public static class InvalidEnumArgumentExceptionExts
+{
+    extension(InvalidEnumArgumentException @this)
+    {
+        public static InvalidEnumArgumentException Create<T>(T value)
+            where T : struct, Enum =>
+            new($"Invalid {typeof(T).GetName()} value: {value}");
+
+        [DoesNotReturn, ContractAnnotation("=> halt")]
+        public static void Throw<T>(T value)
+            where T : struct, Enum =>
+            throw Create(value);
+    }
+}
