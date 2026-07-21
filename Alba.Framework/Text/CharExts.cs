d5329641dd3d6f0291a175c80e93755671b61bba
Athari <athari.p@gmail.com>
2026-07-21 19:15:32 +0300

    Added CharExts with case methods. Reverted to less broken CommunityToolkit.Diagnostics.

---
+++ Alba.Framework/Text/CharExts.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Alba.Framework.Text;
+
+public static class CharExts
+{
+    private static readonly CultureInfo Inv = CultureInfo.InvariantCulture;
+
+    extension(char @this)
+    {
+        public char ToLower() => char.ToLower(@this);
+        public char ToLowerInv() => char.ToLower(@this, Inv);
+        public char ToUpper() => char.ToUpper(@this);
+        public char ToUpperInv() => char.ToUpper(@this, Inv);
+    }
+}
